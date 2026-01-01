5512bd22c6ed113f6bf41325d877c2beb2c46e97
Lo03eR <10lo03er01@gmail.com>
2026-01-01 20:12:23 +0500

    main

--- main.lua
+++ main.lua
@@ -1,36 +1,11 @@
-print("main загружен")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/config.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/functions.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Aimbot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Visual.lua"))()
 
-local function GetFile(File)
-    -- Используем твой путь, но добавляем проверку кэша (?v=...)
-    local Success, Content = pcall(function()
-        return game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/" .. File .. "?v=" .. tick())
-    end)
-    if Success then return Content else return nil end
-end
+-- 5. Запускаем всё!
+getgenv().TuffAimbot.Init() -- Запускаем цикл аимбота
 
--- 1. Загружаем Конфиг (ОБЯЗАТЕЛЬНО ПЕРВЫМ)
-local ConfigSrc = GetFile("Config.lua")
-if ConfigSrc then loadstring(ConfigSrc)() end
-
--- Ждем чуть-чуть, чтобы таблица TuffConfig точно появилась в памяти
-repeat task.wait() until getgenv().TuffConfig
-
--- 2. Загружаем остальные модули
-local FuncsSrc = GetFile("Functions.lua")
-if FuncsSrc then loadstring(FuncsSrc)() end
-
-local AimSrc = GetFile("Aimbot.lua")
-if AimSrc then loadstring(AimSrc)() end
-
-local VisSrc = GetFile("Visuals.lua")
-if VisSrc then loadstring(VisSrc)() end
-
--- 3. Запускаем GUI и логику
-if getgenv().TuffAimbot then
-    getgenv().TuffAimbot.Init()
-end
-
--- Сюда вставляй свой код Rayfield...
 -- 6. Подключаем меню Rayfield
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({ Name = "TuffHub Modular", LoadingTitle = "Запуск..." })
