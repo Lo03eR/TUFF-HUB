f936a77e30b3e0dc424625cd22f4afefe6772aa5
Lo03eR <10lo03er01@gmail.com>
2026-01-01 19:54:23 +0500

    mainn

--- main.lua
+++ main.lua
@@ -1,14 +1,7 @@
--- 1. Загружаем настройки
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Config.lua'))()
-
--- 2. Загружаем функции (поиск цели, проверка стен)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Functions.lua'))()
-
--- 3. Загружаем Аимбот (теперь это отдельный модуль)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Aimbot.lua'))()
-
--- 4. Загружаем Визуалы
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Visual.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Config.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Functions.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Aimbot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lo03eR/TUFF-HUB/refs/heads/main/Visuals.lua"))()
 
 -- 5. Запускаем всё!
 getgenv().TuffAimbot.Init() -- Запускаем цикл аимбота
